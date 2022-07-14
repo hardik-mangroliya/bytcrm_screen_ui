20f1b42a69c3baed06a5807a8b15ed16be328075
hardik mangroliya <hardik.mangroliya@joflee.com>
2022-07-14 18:31:01 +0530

    first screen complete

---
+++ android/app/src/main/kotlin/com/example/bytcrm_screen_ui/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.bytcrm_screen_ui
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:bytcrm_screen_ui/module/home/view/screen/home_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(scaffoldBackgroundColor: Colors.white),
+      home: const HomeScreen(),
+    );
+  }
+}
