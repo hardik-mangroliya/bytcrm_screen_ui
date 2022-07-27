08c5fb8b9786b758c8bdd7fe78ecfcb9d4aca68f
hardik-mangroliya <104610264+hardik-mangroliya@users.noreply.github.com>
2022-07-27 14:45:17 +0530

    Update controller.dart

--- lib/Module/Home/Controller/controller.dart
+++ lib/Module/Home/Controller/controller.dart
@@ -5,23 +5,8 @@ import '../../../Enum/enum.dart';
 class HomeScreenController extends GetxController {
   PageController pageController = PageController();
 
-  // AddEmployeEnum personalDetailScreenStatus = AddEmployeEnum.inProgress;
 
   Rx<Myenum> EmployeList = Rx<Myenum>(Myenum.first);
 
-  // Rx<AddEmployeEnum> personalDetailScreenStatus =
-  //     Rx<AddEmployeEnum>(AddEmployeEnum.inActive);
-  // Rx<AddEmployeEnum> joiningDetailScreenStatus =
-  //     Rx<AddEmployeEnum>(AddEmployeEnum.inActive);
-  // Rx<AddEmployeEnum> employePermissionScreenStatus =
-  //     Rx<AddEmployeEnum>(AddEmployeEnum.inActive);
 }
 
-
-// class Emp{
-//  Emp(this.name,this.salary);
-//   String name; 
-//   double salary;
-
-  
-// }
